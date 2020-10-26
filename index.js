79c1f2bcd14f63b93393a62f53491017aee8c6c1
Oliver Siedler <>
2020-10-26 17:43:11 +0100

    added xaza as qrxtend customer

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ let cards = {
 
 let qrxtendCustomers = {
     antik: "https://qr-xtend.de/show_file_preview/96ef98be-fa47-4e33-860c-dc4bc187241e",
+    xaza: "https://qr-xtend.de/show_file_preview/5e3e0cac-3bbe-4663-a4d9-d359c49777b3",
 };
 redirectToQrxtend();
 
